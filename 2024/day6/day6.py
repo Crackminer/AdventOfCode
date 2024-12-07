b0f7f1a511532f17fe7fb3a73df9bcf527f753a4
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-12-07 15:39:47 +0100

    did day 6 part 2 thanks to tessferrandez and did day7

--- 2024/day6/day6.py
+++ 2024/day6/day6.py
@@ -2,6 +2,9 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2024/day6/day6.txt', 'r') as file:
     puzzleinput = file.read()
 
+# my part 2 was not working, once again thanks to TessFerrandez i was able to finish this. Part 1 i was able to finish myself though, Luckily.
+# Github of TessFerrandez: https://github.com/TessFerrandez/AdventOfCode-Python/blob/develop/2024/day6.py
+
 def makestep(field :list) -> None:
     for i in range(len(field)):
         for j in range(len(field[i])):
@@ -60,14 +63,97 @@ def countX(field :list) -> int:
 def part1():
     input = puzzleinput
     field = []
-    for line in puzzleinput.split('\n'):
+    for line in input.split('\n'):
         field.append(line)
     while not gameended(field):
         makestep(field)
     print(countX(field))
 
-def part2():
+def parse(data):
+    data = data.splitlines()
+    obstacles = set()
+    for y, line in enumerate(data):
+        for x, char in enumerate(line):
+            if char == '#':
+                obstacles.add((x, y))
+            elif char == '^':
+                start = (x, y)
+    width = len(data[0])
+    height = len(data)
+    return obstacles, start, width, height
+
+
+def get_guard_positions(obstacles, start, width, height):
+    visited = set()
+    x, y = start
+    direction = -1j
+
+    while 0 <= x < width and 0 <= y < height:
+        visited.add((x, y))
+        next_pos = (x + direction.real, y + direction.imag)
+        while next_pos in obstacles:
+            # turn right
+            direction = direction * 1j
+            next_pos = (x + direction.real, y + direction.imag)
+        x, y = next_pos
+
+    return visited
+
+
+def is_loop(obstacles, start, width, height):
+    visited = set()
+    x, y = start
+    direction = -1j
+
+    while 0 <= x < width and 0 <= y < height:
+        if ((x, y), direction) in visited:
+            return True
+        visited.add(((x, y), direction))
+        next_pos = (x + direction.real, y + direction.imag)
+        while next_pos in obstacles:
+            # turn right
+            direction = direction * 1j
+            next_pos = (x + direction.real, y + direction.imag)
+        x, y = next_pos
+
+    return False
+
+def part2(obstacles, start, width, height):
+    guard_positions = get_guard_positions(obstacles, start, width, height)
+    guard_positions.remove(start)
+
+    good_positions = set()
+    i = 0
+    for position in guard_positions:
+        i += 1
+        if i % 100 == 0:
+            print(".", end="")
+        obstacles.add(position)
+        if is_loop(obstacles, start, width, height):
+            good_positions.add(position)
+        obstacles.remove(position)
     print()
 
+    return len(good_positions)
+
+
+def test():
+    data = '''....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...'''
+    obstacles, start, width, height = parse(data)
+    assert part2(obstacles, start, width, height) == 6
+
+test()
+data = puzzleinput
+obstacles, start, width, height = parse(data)
+print('Part2:', part2(obstacles, start, width, height))
+
 part1()
-part2()
--- 2024/day7/day7.py
+++ 2024/day7/day7.py
@@ -2,10 +2,76 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2024/day7/day7.txt', 'r') as file:
     puzzleinput = file.read()
 
+testinput = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+
+def getpossibleresults(list :list) -> list:
+    if len(list) == 1:
+        return list
+    results = getpossibleresults(list[:-1])
+    newresults = []
+    for result in results:
+        newresults.append(list[-1] * result)
+        newresults.append(list[-1] + result)
+    return newresults
+
+def possible(list :list) -> bool:
+    target = list[0]
+    results = getpossibleresults(list[1])
+    return target in results
+    
+def getpossibleresultspart2(list :list) -> list:
+    if len(list) == 1:
+        return list
+    results = getpossibleresultspart2(list[:-1])
+    newresults = []
+    for result in results:
+        newresults.append(list[-1] * result)
+        newresults.append(list[-1] + result)
+        newresults.append(int(f'{result}{list[-1]}'))
+    return newresults
+
+def possiblepart2(list :list) -> bool:
+    target = list[0]
+    results = getpossibleresultspart2(list[1])
+    return target in results
+
 def part1():
-    print()
+    input = puzzleinput
+    equations = []
+    for line in input.split('\n'):
+        solution = line.split(': ')[0]
+        numbers = []
+        for numchar in line.split(': ')[1].split(' '):
+            numbers.append(int(numchar))
+        equations.append([int(solution), numbers])
+    totalsum = 0
+    for equation in equations:
+        if possible(equation):
+            totalsum += equation[0]
+    print(totalsum)
 
 def part2():
+    input = puzzleinput
+    equations = []
+    for line in input.split('\n'):
+        solution = line.split(': ')[0]
+        numbers = []
+        for numchar in line.split(': ')[1].split(' '):
+            numbers.append(int(numchar))
+        equations.append([int(solution), numbers])
+    totalsum = 0
+    for equation in equations:
+        if possiblepart2(equation):
+            totalsum += equation[0]
+    print(totalsum)
     print()
 
 part1()
