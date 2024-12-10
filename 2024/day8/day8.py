25d0baff9fe5872b408cc9804df67251ab8d92f8
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-12-10 16:01:20 +0100

    did day 8 and am almost done with day 9 i hope but i have no motivation to continue right now, will finish it later

--- 2024/day8/day8.py
+++ 2024/day8/day8.py
@@ -2,10 +2,101 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2024/day8/day8.txt', 'r') as file:
     puzzleinput = file.read()
 
+testinput = """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"""
+
+def makefrequencieoverlaps(frequencylist :list, overlaplist :list, part2 :bool):
+    for i in range(len(frequencylist)):
+        for j in range(len(frequencylist[i])):
+            if frequencylist[i][j] != '.':
+                for y in range(len(frequencylist)):
+                    for x in range(len(frequencylist[y])):
+                        if frequencylist[i][j] == frequencylist[y][x]:
+                            deltax = j - x
+                            deltay = i - y
+
+                            if deltax == 0 and deltay == 0:
+                                continue
+
+                            if part2:
+                                curx = j
+                                cury = i
+                                while cury + deltay >= 0 and cury + deltay < len(frequencylist) and curx + deltax >= 0 and curx + deltax < len(frequencylist[cury + deltay]):
+                                    cury += deltay
+                                    curx += deltax
+                                string = overlaplist[cury][:curx] + '#' + overlaplist[cury][curx +1:]
+                                overlaplist[cury] = string
+                                while cury - deltay >= 0 and cury - deltay < len(frequencylist) and curx - deltax >= 0 and curx - deltax < len(frequencylist[cury - deltay]):
+                                    cury -= deltay
+                                    curx -= deltax
+                                    string = overlaplist[cury][:curx] + '#' + overlaplist[cury][curx +1:]
+                                    overlaplist[cury] = string
+                                
+                            else:
+                                if i + deltay == y + deltay + deltay and j + deltax == x + deltax + deltax and i + deltay < len(overlaplist) and j + deltax < len(overlaplist[i+deltay]) and i + deltay >= 0 and j + deltax >= 0:
+                                    string = overlaplist[i+deltay][:j+deltax] + '#' + overlaplist[i+deltay][j+deltax +1:]
+                                    overlaplist[i+deltay] = string
+
+                                elif i - deltay == y - deltay - deltay and j - deltax == x - deltax - deltax and i - deltay < len(overlaplist) and j - deltax < len(overlaplist[i-deltay]) and i - deltay >= 0 and j - deltax >= 0:
+                                    string = overlaplist[i-deltay][:j-deltax] + '#' + overlaplist[i-deltay][j-deltax +1:]
+                                    overlaplist[i-deltay] = string
+
+                                if y + deltay == i + deltay + deltay and x + deltax == j + deltax + deltax and y + deltay < len(overlaplist) and x + deltax < len(overlaplist[y+deltay]) and y + deltay >= 0 and x + deltax >= 0:
+                                    string = overlaplist[y+deltay][:x+deltax] + '#' + overlaplist[y+deltay][x+deltax +1:]
+                                    overlaplist[y+deltay] = string
+
+                                elif y - deltay == i - deltay - deltay and x - deltax == j - deltax - deltax and y - deltay < len(overlaplist) and x - deltax < len(overlaplist[y-deltay]) and y - deltay >= 0 and x - deltax >= 0:
+                                    string = overlaplist[y-deltay][:x-deltax] + '#' + overlaplist[y-deltay][x-deltax +1:]
+                                    overlaplist[y-deltay] = string
+
+
+
+
+def countlocations(list :list) -> int:
+    count = 0
+    for line in list:
+        for char in line:
+            if char == '#':
+                count += 1
+    return count
+
 def part1():
-    print()
+    input = puzzleinput
+    frequencylist = []
+    for line in input.split('\n'):
+        frequencylist.append(line)
+    overlaplist = []
+    for line in frequencylist:
+        string = ''
+        for i in range(len(line)):
+            string += '.'
+        overlaplist.append(string)
+    makefrequencieoverlaps(frequencylist, overlaplist, False)
+    print(countlocations(overlaplist))
 
 def part2():
+    input = puzzleinput
+    frequencylist = []
+    for line in input.split('\n'):
+        frequencylist.append(line)
+    overlaplist = []
+    for line in frequencylist:
+        string = ''
+        for i in range(len(line)):
+            string += '.'
+        overlaplist.append(string)
+    makefrequencieoverlaps(frequencylist, overlaplist, True)
+    print(countlocations(overlaplist))
     print()
 
 part1()
--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -2,8 +2,68 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2024/day9/day9.txt', 'r') as file:
     puzzleinput = file.read()
 
+testinput = "2333133121414131402"
+
+def getsum(list :list) -> int:
+    sum = 0
+    for i in range(len(list)):
+        sum += i * list[i]
+    return sum
+
+def bloat(input :list) -> list:
+    output = []
+    for i in range(len(input)):
+        helper = input[i]
+        while helper != 0:
+            if i % 2 == 0:
+                output.append(int(i / 2))
+            else:
+                output.append(-1)
+            helper -= 1
+    return output
+
+def collapse(list :list) -> list:
+    collapsed = []
+    for i in range(len(list)):
+        if list[i] == -1:
+            while list[-1] == -1:
+                list = list[:-1] 
+            if len(list) <= len(collapsed):
+                break
+            collapsed.append(list[-1])
+            list = list[:-1] 
+        else:
+            collapsed.append(list[i])
+    return collapsed
+
+def collapsepart2(list :list) -> list:
+    collapsed = []
+    for i in range(len(list)):
+        if list[i] == -1:
+            blockcount = 1
+            while i + blockcount < len(list) and list[i + blockcount] == -1:
+                blockcount += 1
+            while blockcount > 0:
+                findblock = -1
+                while True:
+                    while list[findblock] == -1:
+                        findblock -= 1
+                    blocklength = 1
+                    while (findblock - blocklength) * -1 < len(list) and list[findblock - blocklength] != -1:
+                        blocklength += 1
+                    if blocklength <= blockcount:
+                        # get the found block from the back inside the block at the beginning
+                        break
+
 def part1():
-    print()
+    inputlist = []
+    input = puzzleinput
+    for char in input:
+        inputlist.append(int(char))
+    
+    bloated = bloat(inputlist)
+    collapsed = collapse(bloated)
+    print(getsum(collapsed))
 
 def part2():
     print()
