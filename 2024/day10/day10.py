78350363bbb16ca9bf1b60eb06f442e011afb469
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-12-11 10:08:50 +0100

    finished day 9 with help needed, finished day 10, which was surprisingly easy...

--- 2024/day10/day10.py
+++ 2024/day10/day10.py
@@ -2,10 +2,84 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2024/day10/day10.txt', 'r') as file:
     puzzleinput = file.read()
 
+testinput = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+
+def findnumtrails(list :list, y :int, x :int) -> list:
+    if list[y][x] == 9:
+        return [[y, x]]
+    l = []
+    if x+1 < len(list[y]) and list[y][x+1] == list[y][x] +1:
+        l.extend(findnumtrails(list, y, x+1))
+    if x-1 >= 0 and list[y][x-1] == list[y][x] +1:
+        l.extend(findnumtrails(list, y, x-1))
+    if y+1 < len(list) and list[y+1][x] == list[y][x] +1:
+        l.extend(findnumtrails(list, y+1, x))
+    if y-1 >= 0 and list[y-1][x] == list[y][x] +1:
+        l.extend(findnumtrails(list, y-1, x))
+    return l
+
+def sumup(list :list) -> int:
+    sum = 0
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            if list[i][j] == 0:
+                l = findnumtrails(list, i, j)
+                r = []
+                for element in l:
+                    if element not in r:
+                        r.append(element)
+                sum += len(r)
+    return sum
+
+def findnumtrailsp2(list :list, y :int, x :int) -> int:
+    if list[y][x] == 9:
+        return 1
+    num = 0
+    if x+1 < len(list[y]) and list[y][x+1] == list[y][x] +1:
+        num += findnumtrailsp2(list, y, x+1)
+    if x-1 >= 0 and list[y][x-1] == list[y][x] +1:
+        num += findnumtrailsp2(list, y, x-1)
+    if y+1 < len(list) and list[y+1][x] == list[y][x] +1:
+        num += findnumtrailsp2(list, y+1, x)
+    if y-1 >= 0 and list[y-1][x] == list[y][x] +1:
+        num += findnumtrailsp2(list, y-1, x)
+    return num
+
+def sumupp2(list :list) -> int:
+    sum = 0
+    for i in range(len(list)):
+        for j in range(len(list[i])):
+            if list[i][j] == 0:
+                sum += findnumtrailsp2(list, i, j)
+    return sum
+
+
 def part1():
-    print()
+    input = puzzleinput
+    hike = []
+    for line in input.split('\n'):
+        tmp = []
+        for char in line:
+            tmp.append(int(char))
+        hike.append(tmp)
+    print(sumup(hike))
 
 def part2():
+    input = puzzleinput
+    hike = []
+    for line in input.split('\n'):
+        tmp = []
+        for char in line:
+            tmp.append(int(char))
+        hike.append(tmp)
+    print(sumupp2(hike))
     print()
 
 part1()
--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -24,40 +24,54 @@ def bloat(input :list) -> list:
 
 def collapse(list :list) -> list:
     collapsed = []
-    for i in range(len(list)):
-        if list[i] == -1:
-            while list[-1] == -1:
+    try:
+        for i in range(len(list)):
+            if list[i] == -1:
+                while list[-1] == -1:
+                    list = list[:-1] 
+                if len(list) <= len(collapsed):
+                    break
+                collapsed.append(list[-1])
                 list = list[:-1] 
-            if len(list) <= len(collapsed):
-                break
-            collapsed.append(list[-1])
-            list = list[:-1] 
-        else:
-            collapsed.append(list[i])
-    return collapsed
+            else:
+                collapsed.append(list[i])
+        return collapsed
+    except:
+        return collapsed
 
 def collapsepart2(list :list) -> list:
     collapsed = []
-    for i in range(len(list)):
-        if list[i] == -1:
-            blockcount = 1
-            while i + blockcount < len(list) and list[i + blockcount] == -1:
-                blockcount += 1
-            while blockcount > 0:
-                findblock = -1
-                while True:
-                    while list[findblock] == -1:
-                        findblock -= 1
-                    blocklength = 1
-                    while (findblock - blocklength) * -1 < len(list) and list[findblock - blocklength] != -1:
-                        blocklength += 1
-                    if blocklength <= blockcount:
-                        # get the found block from the back inside the block at the beginning
-                        break
+    try:
+        for i in range(len(list)):
+            if list[i] == -1:
+                blockcount = 1
+                while i + blockcount < len(list) and list[i + blockcount] == -1:
+                    blockcount += 1
+                while blockcount > 0:
+                    findblock = -1
+                    while True:
+                        while list[findblock] == -1:
+                            findblock -= 1
+                        if findblock * -1 >= i:
+                            return collapsed
+                        blocklength = 1
+                        while (findblock - blocklength) * -1 < len(list) and list[findblock - blocklength] != -1:
+                            blocklength += 1
+                        if blocklength <= blockcount:
+                            # get the found block from the back inside the block at the beginning
+                            collapsed.extend(list[findblock - blocklength:findblock])
+                            for j in range(blocklength):
+                                list[findblock - j] = -1
+                            break
+            else:
+                collapsed.append(list[i])
+        return collapsed
+    except:
+        return collapsed
 
 def part1():
     inputlist = []
-    input = puzzleinput
+    input = testinput   # changed to testinput for part 2 as puzzleinput takes about 5-10 seconds and i dont want to wait on that
     for char in input:
         inputlist.append(int(char))
     
@@ -66,7 +80,25 @@ def part1():
     print(getsum(collapsed))
 
 def part2():
-    print()
+    # Thank you u/4HbQ on Reddit for the solution, found in this post: https://www.reddit.com/r/adventofcode/comments/1ha27bo/2024_day_9_solutions/
+    D = [(i//2+1 if i%2 else 0, int(d)) for i,d in enumerate(puzzleinput, 1)]
+
+    for i in range(len(D))[::-1]:
+        for j in range(i):
+            i_data, i_size = D[i]
+            j_data, j_size = D[j]
+
+            if i_data and not j_data and i_size <= j_size:
+                D[i] = (0, i_size)
+                D[j] = (0, j_size - i_size)
+                D.insert(j, (i_data, i_size))
+
+
+    flatten = lambda x: [x for x in x for x in x]
+
+    print(sum(i*(c-1) for i,c in enumerate(flatten([d]*s for d,s in D)) if c))
+
+
 
 part1()
 part2()
