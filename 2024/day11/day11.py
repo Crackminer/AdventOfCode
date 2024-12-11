8f349a6df44d5833d56e73fafe232e9469d2d346
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-12-11 10:23:01 +0100

    day 11 done, this was the simplest day in quite a while, it just takes quite long for part 2 as this is not optimized whatsoever

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -2,11 +2,38 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2024/day11/day11.txt', 'r') as file:
     puzzleinput = file.read()
 
+testinput = "125 17"
+
+def blink(list :list) -> None:
+    newlist = []
+    for element in list:
+        if element == 0:
+            newlist.append(1)
+        elif len(str(element)) % 2 == 0:
+            string = str(element)
+            newlist.append(int(string[:int(len(string) / 2)]))
+            newlist.append(int(string[int(len(string) / 2):]))
+        else:
+            newlist.append(element * 2024)
+    return newlist
+
 def part1():
-    print()
+    input = puzzleinput
+    list = []
+    for numstring in input.split(' '):
+        list.append(int(numstring))
+    for i in range(25):
+        list = blink(list)
+    print(len(list))
 
 def part2():
-    print()
+    input = puzzleinput
+    list = []
+    for numstring in input.split(' '):
+        list.append(int(numstring))
+    for i in range(75):
+        list = blink(list)
+    print(len(list))
 
 part1()
 part2()
