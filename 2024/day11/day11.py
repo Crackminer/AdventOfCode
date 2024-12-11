0bebb0bdaf06d6f6ed92ea248ab38fd761063897
Crackminer <87083544+Crackminer@users.noreply.github.com>
2024-12-11 12:35:26 +0100

    ok, so my previous day 11 part 2 solution did not work, i have too little ram for that. it is solved thanks to thomasjevskij as i would never have come up with that solution on my own. I also learned there is a cache feature in python, so that is neat

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,21 +1,48 @@
+import functools
+
 puzzleinput :str        # done to initialize an empty variable, without the :str it would be an error
 with open('2024/day11/day11.txt', 'r') as file:
     puzzleinput = file.read()
 
 testinput = "125 17"
 
-def blink(list :list) -> None:
+def blink(list :list) -> list:
     newlist = []
     for element in list:
-        if element == 0:
-            newlist.append(1)
-        elif len(str(element)) % 2 == 0:
-            string = str(element)
-            newlist.append(int(string[:int(len(string) / 2)]))
-            newlist.append(int(string[int(len(string) / 2):]))
+        num = getnum(element)
+        if type(num) is int:
+            newlist.append(num)
         else:
-            newlist.append(element * 2024)
+            newlist.append(num[0])
+            newlist.append(num[1])
     return newlist
+
+@functools.cache
+def getnum(num :int) -> int | tuple:
+    if num == 0:
+        return 1
+    elif len(str(num)) % 2 == 0:
+        string = str(num)
+        return int(string[:int(len(string) / 2)]), int(string[int(len(string) / 2):])
+    else:
+        return num * 2024
+    
+# thanks to thomasjevskij, because i would not have gotten this myself at all. My former brute force method went out of ram and stopped to process after hitting about 58GB of ram for like the 10th time. https://github.com/thomasjevskij/advent_of_code/blob/master/2024/aoc11/day11.py
+@functools.cache
+def blinkp2(number):
+    if number == 0:
+        return [1]
+    s = f'{number}'
+    l = len(s)
+    if l % 2 == 0:
+        return [int(n) for n in [s[:l//2], s[l//2:]]]
+    return [number * 2024]
+
+@functools.cache
+def countsplits(number, blinks):
+    if blinks == 0:
+        return 1
+    return sum(countsplits(num, blinks -1) for num in blinkp2(number))
 
 def part1():
     input = puzzleinput
@@ -31,9 +58,8 @@ def part2():
     list = []
     for numstring in input.split(' '):
         list.append(int(numstring))
-    for i in range(75):
-        list = blink(list)
-    print(len(list))
+    print(sum(countsplits(num, 75) for num in list))
 
 part1()
+print('\n\n\n')
 part2()
