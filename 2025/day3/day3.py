c68f3e58e891ec62cecc65eff7f348fd02b7bd70
Patrick Schewe <87083544+Crackminer@users.noreply.github.com>
2025-12-04 06:42:18 +0100

    AoC 2025 day 4 input and day3 solution

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -2,15 +2,38 @@ puzzleinput :str        # done to initialize an empty variable, without the :str
 with open('2025/day3/day3.txt', 'r') as file:
     puzzleinput = file.read()
 
-testinput = """"""
+testinput = """987654321111111
+811111111111119
+234234234234278
+818181911112111"""
+
+def meanFunc(numdigits):
+    #puzzleinput = testinput
+    sum = 0
+    for line in puzzleinput.splitlines():
+        indexes = []
+        for i in range(numdigits, 0, -1):
+            curHigh = int(line[len(line)-i])
+            index = len(line)-i
+            for j in range(len(line)-i, numdigits-1-i, -1):
+                indexes.sort()
+                if int(line[j]) >= curHigh and (len(indexes) == 0 or j > indexes[-1]):
+                    curHigh = int(line[j])
+                    index = j
+            indexes.append(index)
+        num = 0
+        indexes.sort()
+        for index in indexes:
+            num = num * 10 + int(line[index])
+        sum += num
+
+    print(sum)
 
 def part1():
-    # puzzleinput = testinput
-    print()
+    meanFunc(2)
 
 def part2():
-    # puzzleinput = testinput
-    print()
+    meanFunc(12)
 
 part1()
 part2()
